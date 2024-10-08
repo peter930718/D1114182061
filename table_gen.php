5967197f4a903ca45e02609a5d779cb3fe15cc44
peter930718 <peter93342004@gmail.com>
2024-10-08 11:51:41 +0800

    Update table_gen.php

--- table_gen.php
+++ table_gen.php
@@ -1,7 +1,27 @@
 <?php
 
   $row_count = $_GET['row_num'];
-  $password = $_GET['col_num'];
+  $col_count = $_GET['col_num'];
+  $gender = $_GET['gender'];
+  $dept = $_GET['dept'];
+  $msg = $_GET['message'];
 
-  echo'表格大小:',$account,'，密碼:',$password,'，驗證碼',$check_code;
+
+  echo '表格大小:'. $row_count . '列，' . $col_count . '行。';
+  echo '性別:' . $gender . "。";
+  echo '科系:' . $dept . "。";
+  echo '留言:' . $msg . "。";
+
+
+  echo '<table border=\"1\">';
+
+  for ($i = 0;$i < $row_count; $i++){
+    echo '<tr>';
+    for ($j = 0; $j < $col_count; $j++){
+      echo '<td>行' . ($i + 1) . ' 列' . ($j + 1) . '</td>';
+    }
+    echo '<tr>';
+  }
+
+  echo '</table>';
 ?>
